215fb7b7138d1ea77530509ca933fc0846c84341
Lucas <lucascraveiropaes@gmail.com>
2022-08-10 20:41:17 -0300

    Arquivos iniciais

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fake Posts
+
+Aqui tem as descricoes tecnicas desse projeto. Na pasta ```assets``` ficam os arquivos de midia, css, e javascript.
+
+## Scripts
+
+| Comando | Descricao |
+| :------------- | :------------- |
+| ```npm run start``` | Inicia o servidor de dev  |
+
+[Link de Referencia do Blog](https://www.uplabs.com/posts/blog-website-html-template)
---
+++ assets/js/scripts.js
@@ -0,0 +1,25 @@
+function randomImage() {
+  const max = 5;
+  const min = 1;
+  const num = parseInt(Math.random() * (max - min) + min);
+
+  return `assets/img/${num}.webp`;
+}
+
+document.addEventListener("DOMContentLoaded", async function() {
+  const raw = await fetch("http://jsonplaceholder.typicode.com/posts");
+  const res = await raw.json();
+
+  for (let i = 0; i < res.length; i++) {
+    const post = res[i];
+
+    const card = document.querySelector(".card").cloneNode(true);
+
+    card.children.item(0).innerHTML = post.title;
+    card.children.item(1).innerHTML = post.body;
+
+    card.style.backgroundImage = `url(${ randomImage() })`;
+
+    document.querySelector("section").appendChild(card);
+  }
+});
